9be80d6bfc9dad047354599a795f9549a5bfbfea
Vadim <monstr518@ukr.net>
2021-12-28 19:54:30 +0200

    Add files via upload

---
+++ computerating.js
@@ -0,0 +1,46 @@
+// computerating.js
+
+
+// Вычисление рейтинга Эло.
+// RAB - [RA,RB] - Рейтинги игроков до игры.
+// NAB - [NA,NB] - Количества сыгранных игр до этой.
+// SA - фактически набранное игроком A количество очков (1 очко за победу, 0,5 — за ничью и 0 — за поражение).
+var ComputeRatingELO = (RAB,NAB,SA)=>{
+	var SB = 1 - SA;
+	
+	// Количество очков, которое наберёт игрок A в партии с B.
+	var EA = (RA,RB)=>{
+		var futer = 1+Math.pow(10,RB-RA);
+		return 1 / futer;
+		};
+		
+	// Коэффициент. Возраст здесь не учитывается.
+	// R - Рейтинг игрока до игры.
+	// N - Количество сигранных до этой игры игр.
+	var K = (R,N)=>{
+		if(N<=30)return 40;
+		if(R>=2400)return 10;
+		return 20;
+		};
+	
+	var NewRA = RAB[0] + K(RAB[0],NAB[0])*(SA-EA(RAB[0],RAB[1]));
+	var NewRB = RAB[1] + K(RAB[1],NAB[1])*(SB-EA(RAB[1],RAB[0]));
+	
+	return [NewRA,NewRB]; // Новый RAB, после этой игры.
+};
+
+
+
+/*
+var test = ()=>{
+	console.log(ComputeRatingELO([0,0],[0,0],1)); // [ 20, -20 ]
+	console.log(ComputeRatingELO([100,100],[4,2],0)); // [ 80, 120 ]
+	console.log(ComputeRatingELO([100,100],[4,2],0.5)); // [ 100, 100 ]
+};
+
+test();
+*/
+
+
+// Для NAB - [NA,NB] нужно добавить по единичке.
+module.exports = ComputeRatingELO;
